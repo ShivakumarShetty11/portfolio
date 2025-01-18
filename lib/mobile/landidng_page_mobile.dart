4a506e789cd85189a277b34b189014aa2efdf229
ShivakumarShetty11 <shivakumar.vahani@gmail.com>
2025-01-18 07:26:23 +0530

    first commit

---
+++ lib/mobile/landidng_page_mobile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LandidngPageMobile extends StatefulWidget {
+  const LandidngPageMobile({super.key});
+
+  @override
+  State<LandidngPageMobile> createState() => LandidngPageMobileState();
+}
+
+class LandidngPageMobileState extends State<LandidngPageMobile> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/web/landidng_page_web.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio/components.dart';
+
+class LandingPageWeb extends StatefulWidget {
+  const LandingPageWeb({super.key});
+
+  @override
+  State<LandingPageWeb> createState() => _LandingPageWebState();
+}
+
+class _LandingPageWebState extends State<LandingPageWeb> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.greenAccent,
+        title: TabsWeb(),
+      ),
+    );
+  }
+}
