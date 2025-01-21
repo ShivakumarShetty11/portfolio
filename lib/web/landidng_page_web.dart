242b172f31f08bbe28b191d3e68446ab40c04dea
ShivakumarShetty11 <shivakumar.vahani@gmail.com>
2025-01-21 07:33:02 +0530

    2

--- lib/web/landidng_page_web.dart
+++ lib/web/landidng_page_web.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:portfolio/components.dart';
 
 class LandingPageWeb extends StatefulWidget {
@@ -21,23 +22,75 @@ class LandingPageWebState extends State<LandingPageWeb> {
         iconTheme: IconThemeData(size: 25.0, color: Colors.black),
         title: TabsWeb("HOME"),
       ),
-      body: ListView(children: [
-        Container(
-            child: Row(children: [
-          CircleAvatar(
-              radius: 146.0,
-              backgroundColor: Colors.tealAccent,
-              child: CircleAvatar(
-                radius: 143.0,
-                backgroundColor: Colors.black,
-                child: CircleAvatar(
-                  radius: 140.0,
-                  backgroundColor: Colors.white,
-                  backgroundImage: AssetImage("assets/image1circle.png"),
+      body: ListView(
+        padding: EdgeInsets.all(16.0),
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 20.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                // Image Section
+                CircleAvatar(
+                  radius: 146.0,
+                  backgroundColor: Colors.tealAccent,
+                  child: CircleAvatar(
+                    radius: 143.0,
+                    backgroundColor: Colors.black,
+                    child: CircleAvatar(
+                      radius: 140.0,
+                      backgroundColor: Colors.white,
+                      backgroundImage: AssetImage("assets/image1circle.png"),
+                    ),
+                  ),
                 ),
-              ))
-        ])),
-      ]),
+
+                SizedBox(width: 20.0), // Space between image and text
+
+                // Text Section
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.all(16.0),
+                    decoration: BoxDecoration(
+                      color: Colors.tealAccent,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20.0),
+                        bottomRight: Radius.circular(20.0),
+                      ),
+                    ),
+                    child: Text(
+                      "Hello, I'm here!",
+                      style: GoogleFonts.oswald(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.black,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class TabsWeb extends StatelessWidget {
+  final String title;
+  TabsWeb(this.title);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      title,
+      style: TextStyle(
+        fontSize: 20.0,
+        fontWeight: FontWeight.bold,
+        color: Colors.black,
+      ),
     );
   }
 }
